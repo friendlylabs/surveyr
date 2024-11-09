984d4dcd1c50c9a8c4f1d668bc19af0027196bef
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-09 22:57:24 +0300

    add: public directory

---
+++ public/assets/js/app.js
@@ -0,0 +1,186 @@
+// Event Listeners
+$('.togglePassword').click(function() {
+    $(this).toggleClass('ph-eye').toggleClass('ph-eye-slash');
+    var input = $($(this).prev('input'));
+    if (input.attr('type') === 'password') {
+        input.attr('type', 'text');
+    } else {
+        input.attr('type', 'password');
+    }
+});
+
+// Global Functions
+function buttonState(button, state, initialText=null) {
+
+    button = $(button);
+
+    if (state === 'loading') {
+        button.attr('disabled', true);
+        button.html('<i class="fas fa-spinner fa-spin"></i>');
+    } else {
+        button.attr('disabled', false);
+        button.html(initialText);
+    }
+}
+
+
+function underDevelopment(){
+    // prevent default action
+    event.preventDefault();
+
+    Swal.fire({
+        title: 'Under Development',
+        text: 'This feature is under development and will be available soon.',
+        icon: 'info',
+        confirmButtonText: 'OK'
+    });
+}
+
+
+// Styles and Scripts Injection
+function injectStylesheet(url) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = url;
+    document.head.appendChild(link);
+}
+
+function injectScript(url) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+    });
+}
+
+function copyToClipboard(text) {
+    var input = document.createElement('input');
+    input.value = text;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+
+    toast.info({ message: 'Copied to clipboard' });
+}
+
+function submitForm(event, responseHandler = null){
+    event.preventDefault();
+
+    const form = $(event.target); // Get the form element
+    const isMultipart = form.attr('enctype') === 'multipart/form-data'; // Check if it's a multipart form
+
+    // Create a FormData object if multipart, else serialize the form
+    let formData;
+    if (isMultipart) {
+        formData = new FormData(event.target);
+    } else {
+        formData = form.serialize();
+    }
+
+    // find the submit button
+    const submitButton = form.find('button[type="submit"]');
+    const buttonLabel = submitButton.html() ?? null;
+    
+    if (submitButton) {
+        buttonState(submitButton, 'loading');
+    }
+
+    $.ajax({
+        url: form.attr('action'),
+        method: form.attr('method') ?? 'POST',
+        data: formData,
+        processData: !isMultipart, // Don't process the data if it's multipart (FormData handles that)
+        contentType: isMultipart ? false : 'application/x-www-form-urlencoded; charset=UTF-8', // Set content type accordingly
+        success: function(response) {
+            if (responseHandler && typeof responseHandler === 'function') {
+                responseHandler(response);
+            }else{
+                if (response.status) {
+                    toast.success({ message: response.message });
+                }else{
+                    toast.error({ message: response.message ?? 'An Unknown error occured' });
+                }
+
+                if(response.redirect){
+                    setTimeout(() => {
+                        window.location.href = response.redirect;
+                    }, 1000);
+                }
+            }
+        },
+        error: function() {
+            toast.error({ message: 'Unknown Error Occurred' });
+        },
+        complete: function() {
+            // Reset button state if buttonId is provided
+            if (submitButton) {
+                buttonState(submitButton, 'reset', buttonLabel);
+            }
+        }
+    });
+}
+
+// anchor tag click event
+function confirmDelete(event){
+
+    event.preventDefault();
+    const buttonHref = event.target.href;
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: 'You will not be able to recover this record!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        confirmButtonColor: '#dc3545',
+        cancelButtonColor: '#6c757d'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = buttonHref;
+        }
+    });
+}
+
+// initializers and settings
+const toast = iziToast;
+toast.settings({
+    timeout: 1500,
+    transitionIn: 'fadeInDown',
+    transitionOut: 'fadeOutUp',
+    position: 'bottomCenter',
+    close: true,
+    progressBar: true,
+    pauseOnHover: true
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // flatpickr datepicker
+    if(document.querySelector('.date-selector')) {
+        document.querySelectorAll('.date-selector').forEach(function(input) {
+            flatpickr(input, {
+                dateFormat: "Y-m-d", // Match format with server-side value
+                defaultDate: input.value // Set each input's initial value as defaultDate
+            });
+        });
+    }
+
+    // bootstrap select
+    if(document.querySelector('.form-selector')) {
+        $('.form-selector').selectpicker();
+    }
+
+    // listent to all .fs-* classes and assign font-size dynamically
+    $('[class*="fs-"]').each(function() {
+        var classes = $(this).attr('class').split(' ');
+        var fontSize = classes.find(c => c.startsWith('fs-'));
+        var size = fontSize.split('-')[1];
+        $(this).css('font-size', size + 'px');
+    });
+
+});
---
+++ public/assets/js/layout.js
@@ -0,0 +1,109 @@
+var navbarTopShape = window.config.config.phoenixNavbarTopShape;
+var navbarPosition = window.config.config.phoenixNavbarPosition;
+var body = document.querySelector('body');
+var navbarDefault = document.querySelector('#navbarDefault');
+var navbarTop = document.querySelector('#navbarTop');
+var topNavSlim = document.querySelector('#topNavSlim');
+var navbarTopSlim = document.querySelector('#navbarTopSlim');
+var navbarCombo = document.querySelector('#navbarCombo');
+var navbarComboSlim = document.querySelector('#navbarComboSlim');
+var dualNav = document.querySelector('#dualNav');
+
+var documentElement = document.documentElement;
+var navbarVertical = document.querySelector('.navbar-vertical');
+
+if (navbarTopShape === 'default' && navbarPosition === 'horizontal') {
+    navbarDefault?.remove();
+    topNavSlim?.remove();
+    navbarVertical?.remove();
+    navbarTopSlim?.remove();
+    navbarCombo?.remove();
+    navbarComboSlim?.remove();
+    dualNav?.remove();
+    navbarTop.removeAttribute('style');
+    document.documentElement.setAttribute('data-navigation-type', 'horizontal');
+}
+
+var navbarTopStyle = window.config.config.phoenixNavbarTopStyle;
+var navbarTop = document.querySelector('.navbar-top');
+if (navbarTopStyle === 'darker') {
+    navbarTop.setAttribute('data-navbar-appearance', 'darker');
+}
+
+var navbarVerticalStyle = window.config.config.phoenixNavbarVerticalStyle;
+var navbarVertical = document.querySelector('.navbar-vertical');
+if (navbarVerticalStyle === 'darker') {
+    navbarVertical.setAttribute('data-navbar-appearance', 'darker');
+}
+
+// add active to current page
+var currentUrl = window.location.href;
+var navLinks = document.querySelectorAll('.navbar-nav a');
+navLinks.forEach(function (navLink) {
+    if (navLink.href === currentUrl) {
+        navLink.classList.add('active');
+
+        // check if navLink is in a dropdown
+        var dropdown = navLink.closest('.dropdown');
+        if (dropdown) {
+            // add to dropdown-toggle
+            dropdown.querySelector('.dropdown-toggle').classList.add('active');
+        }
+    }
+});
+
+function menuToOffcanvas() {
+    // check if offCanvas #mobileMenu exists, if not create it
+    var offCanvas = document.querySelector('#mobileMenu');
+    var sideBarContent = document.getElementById('navbarTopCollapse')
+
+    if (!offCanvas) {
+        var offCanvas = document.createElement('div');
+        offCanvas.id = 'mobileMenu';
+        offCanvas.classList.add('offcanvas', 'offcanvas-start');
+        offCanvas.style.maxWidth = '320px';
+        offCanvas.innerHTML = `
+            <div class="offcanvas-header border-bottom" style="height: 65px;">
+                <img id="mobileMenuLogo" src="/assets/images/brand/logo-dark.png" alt="logo" height="40" />
+                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body navbar-mobile">
+                ${sideBarContent.innerHTML}
+            </div>
+        `;
+        document.body.appendChild(offCanvas);
+        console.log('offCanvas created');
+
+        //menuToOffcanvas();
+        setTimeout(() => { menuToOffcanvas(); }, 100);
+    }else{
+
+        var appTheme = localStorage.getItem('phoenixTheme');
+        var logo = document.getElementById('mobileMenuLogo');
+        if (appTheme === 'dark') {
+            logo.src = '/assets/images/brand/logo-light.png';
+        } else {
+            logo.src = '/assets/images/brand/logo-dark.png';
+        }
+
+        var offCanvas = document.getElementById('mobileMenu');
+        var mobileMenuCanvas = new bootstrap.Offcanvas(offCanvas);
+
+        // check if offCanvas has .show class
+        if (offCanvas.classList.contains('show')) {
+
+            if(bootstrap.Offcanvas.getInstance(offCanvas).isShown){
+                mobileMenuCanvas.hide();
+            }else{
+                // overide the default offcanvas hide method
+                offCanvas.classList.remove('show');
+                var backdrop = document.querySelector('.offcanvas-backdrop');
+                backdrop.remove();
+            }
+
+        } else {
+            mobileMenuCanvas.show();
+        }
+    }
+
+}
