dac1d83547c2f5e7ea4056cd3441492e3ec5748e
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-11 20:24:43 +0300

    add: userroles seeder

---
+++ app/database/seeds/UserRolesSeeder.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Database\Seeds;
+
+use App\Models\UserRole;
+use Illuminate\Database\Seeder;
+
+class UserRolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * @return void
+     */
+    public function run()
+    {
+        UserRole::truncate();
+
+        UserRole::create([
+            'name' => 'admin',
+            'description' => 'Administrator',
+            'is_admin' => true
+        ]);
+
+        UserRole::create([
+            'name' => 'moderator',
+            'description' => 'Moderator',
+            'is_admin' => false
+        ]);
+
+        UserRole::create([
+            'name' => 'user',
+            'description' => 'User',
+            'is_admin' => false
+        ]);
+    }
+}
