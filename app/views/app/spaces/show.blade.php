8b2656e92e1a93c5c3c482859b05ad5da0424a01
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-11 20:30:30 +0300

    refactor: update space edit modal in space show view

--- app/views/app/spaces/show.blade.php
+++ app/views/app/spaces/show.blade.php
@@ -38,4 +38,44 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="editSpaceModal" tabindex="-1" role="dialog" aria-labelledby="editSpaceModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editSpaceModalLabel">Edit Space</h5>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('spaces.update') }}" method="post" id="editSpaceForm" onsubmit="submitForm(event)">
+                        @csrf
+                        <input type="hidden" name="spaceId" value="{{ $space->id }}">
+                        
+                        <div class="mb-3">
+                            <label for="spaceName" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="spaceName" name="spaceName" value="{{ $space->name }}" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="spaceDescription" class="form-label">Description</label>
+                            <textarea class="form-control" id="spaceDescription" name="spaceDescription" rows="3" required>{{ $space->description }}</textarea>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="spaceMembers" class="form-label d-block mb-2">Members</label>
+                            <select class="form-selector" id="spaceMembers" name="spaceMembers[]" data-live-search="true" title="select space collaborators" multiple>
+                                @foreach($users as $user)
+                                    <option value="{{ $user->id }}" {{ in_array($user->id, $space->members) ? 'selected' : '' }}>{{ $user->fullname }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary w-100">Update Space</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
