41053191d6cd1def6ad2b335bf54a7986056dcf4
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-11 20:32:05 +0300

    refactor: update sidebar to include user management link for admin and moderator roles

--- app/views/layouts/app/partials/sidebar.blade.php
+++ app/views/layouts/app/partials/sidebar.blade.php
@@ -1,13 +1,13 @@
 <div class="collapse navbar-collapse navbar-top-collapse order-1 order-lg-0 justify-content-center" id="navbarTopCollapse">
     <ul class="navbar-nav navbar-nav-top" data-dropdown-on-hover="data-dropdown-on-hover">
         <li class="nav-item">
-            <a class="nav-link lh-1" href="@route('app.home')" wire:navigate>
+            <a class="nav-link lh-1" href="@route('app.home')">
                 <i class="fa-solid fa-grid-2 me-2 fs-10"></i>
                 Dashboard
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link lh-1" href="@route('spaces.list')" wire:navigate>
+            <a class="nav-link lh-1 @active('app.spaces')" href="@route('spaces.list')">
                 <i class="fa-solid fa-folders me-2 fs-10"></i>
                 Spaces
             </a>
@@ -20,18 +20,27 @@
             </a>
             <ul class="dropdown-menu navbar-dropdown-caret">
                 <li class="nav-item">
-                    <a class="nav-link dropdown-item" href="@route('forms.list')" wire:navigate>
+                    <a class="nav-link dropdown-item" href="@route('forms.list')">
                         <span class="fa-solid fa-bars-sort me-2 fs-10"></span>
                         List
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link dropdown-item" href="@route('forms.build')" wire:navigate>
+                    <a class="nav-link dropdown-item" href="@route('forms.build')">
                         <i class="fa-solid fa-screwdriver-wrench me-2 fs-10"></i>
                         Buid
                     </a>
                 </li>
             </ul>
         </li>
+
+        @if(in_array($loggedUser['role'], ['admin', 'moderator']))
+            <li class="nav-item">
+                <a class="nav-link lh-1 @active('app.users')" href="@route('users.index')">
+                    <i class="fa-solid fa-users me-2 fs-10"></i>
+                    Users
+                </a>
+            </li>
+        @endif
     </ul>
 </div>
