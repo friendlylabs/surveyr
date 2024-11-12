68309b8f8424b2e6e187c57493a10cc44bfc4967
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-12 22:23:17 +0300

    feat: add survey theme script to form show view

--- app/views/app/forms/show.blade.php
+++ app/views/app/forms/show.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.form.main')
+
 @section('content')
     <div id="surveyContainer" style="height: 100vh;"></div>
 @endsection
+
+@script('/vendor/surveyjs/themes/index.min.js','src')
 @script('app.forms.scripts.show')
