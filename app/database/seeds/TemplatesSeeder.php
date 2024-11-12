f95e2a118315729a2e65a5d2a922f0992fdac14d
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-13 00:02:10 +0300

    add: create TemplatesSeeder and add templates.json for database seeding

---
+++ app/database/seeds/TemplatesSeeder.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Database\Seeds;
+
+use App\Models\Template;
+use App\Database\Factories\TemplateFactory;
+use Illuminate\Database\Seeder;
+
+class TemplatesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * @return void
+     */
+    public function run()
+    {
+        //Template::truncate();
+
+        $templatesRecords = file_get_contents(StoragePath('app/db/templates.json'));
+        $templates = json_decode($templatesRecords, true)['rows'];
+
+        Template::insert($templates);
+    }
+}
