4fcc93ffc1a054a9ad5fd616ccf8ea832d40cd19
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-14 19:43:00 +0300

    feat: update closed form view to conditionally display preview link for authenticated users

--- app/views/app/forms/closed.blade.php
+++ app/views/app/forms/closed.blade.php
@@ -7,7 +7,7 @@
                     <div class="row justify-content-center align-items-center g-5">
                         <div class="col-12 col-lg-6 text-center order-lg-1">                            
                             <img class="img-fluid w-lg-100 d-dark-none" src="/assets/images/vector/teapot.svg" alt="" width="400" />
-                            <img class="img-fluid w-md-50 w-lg-100 d-light-none" src="/assets/images/vector/teapot.png" alt="" width="540" />
+                            <img class="img-fluid w-md-50 w-lg-100 d-light-none" src="/assets/images/vector/teapot.svg" alt="" width="540" />
                         </div>
                         <div class="col-12 col-lg-6 text-center text-lg-start">
                             <div class="error-container mb-1 w-50 w-lg-75 d-none d-lg-block">
@@ -19,7 +19,11 @@
                                 I refuse the attempt to brew coffee with a teapot,
                                 <br class="d-none d-sm-block" /> Uuhm I mean the server refuses to serve the form, as it is not yet open for submissions.
                             </p>
-                            <a class="btn btn-lg btn-primary" href="@route('app.home')">Go Home</a>
+                            @if(auth()->id())
+                                <a class="btn btn-lg btn-primary" href="@route('forms.preview', $formId)">Preview Form</a>
+                            @else
+                                <a class="btn btn-lg btn-primary" href="@route('app.home')">Go Home</a>
+                            @endif  
                         </div>
                     </div>
                 </div>
