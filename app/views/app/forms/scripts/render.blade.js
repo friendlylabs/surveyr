427102b0e164f880ba5db9866c01f01f662233b6
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-23 03:27:24 +0300

    feat: update survey theme application logic to use dynamic theme variables

--- app/views/app/forms/scripts/render.blade.js
+++ app/views/app/forms/scripts/render.blade.js
@@ -1,13 +1,17 @@
 const defaultJson = `{!! json_encode($form->content) !!}`;
 var currentTheme = localStorage.getItem('phoenixTheme') ?? 'light';
 
+var formTheme = "{{ $form->theme }}";
+const SurveyThemeDark = SurveyTheme[formTheme + 'DarkPanelless'];
+const SurveyThemeLight = SurveyTheme[formTheme + 'LightPanelless'];
+
 const survey = new Survey.Model(defaultJson)
 document.addEventListener("DOMContentLoaded", function() {
     survey.render(document.getElementById("surveyContainer"));
     if(currentTheme === 'light'){
-        survey.applyTheme(SurveyTheme.SolidLightPanelless);
+        survey.applyTheme(SurveyThemeLight);
     }else{
-        survey.applyTheme(SurveyTheme.SolidDarkPanelless);
+        survey.applyTheme(SurveyThemeDark);
     }
 });
 
@@ -25,9 +29,9 @@ document.addEventListener('phoenixThemeChanged', () => {
     const newTheme = localStorage.getItem('phoenixTheme');
     currentTheme = newTheme;
     if(currentTheme === 'dark'){
-        survey.applyTheme(SurveyTheme.SolidLightPanelless);
+        survey.applyTheme(SurveyThemeLight);
     }
     else{
-        survey.applyTheme(SurveyTheme.SolidDarkPanelless);
+        survey.applyTheme(SurveyThemeDark);
     }
 });
