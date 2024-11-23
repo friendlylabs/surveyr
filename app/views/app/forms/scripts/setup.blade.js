fd66b50bda50179fb4dc68e6fca1a15c122416e8
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-23 03:27:38 +0300

    feat: add QR code download functionality and theme selection handling in setup script

--- app/views/app/forms/scripts/setup.blade.js
+++ app/views/app/forms/scripts/setup.blade.js
@@ -21,3 +21,49 @@ document.querySelectorAll('.profile-overview-switch').forEach(button => {
         document.querySelector(targetBlock).classList.remove('d-none');
     });
 });
+
+function downloadQRCode(name) {
+    const qrcodeDiv = document.getElementById('qrcode');
+    const img = qrcodeDiv.querySelector('img');
+
+    if (img && img.src) {
+        const link = document.createElement('a');
+        link.href = img.src;
+        link.download = `${name}-qrcode.png`; // Set the filename with the user's name
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link); // Cleanup
+    } else {
+        alert('No image found to download.');
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    $('.theme-card').on('click', function () {
+        const $this = $(this);
+        const selectedTheme = $this.data('theme');
+        
+        // Remove 'bg-danger' from all cards and add it to the selected card
+        $('.theme-card').removeClass('bg-danger');
+        $this.addClass('bg-danger');
+
+        $.ajax({
+            url: "{{ route('forms.setup.update', 'theme', $form->id) }}",
+            type: 'POST',
+            data: {
+                _token: "{{ csrf_token() }}",
+                theme: selectedTheme
+            },
+            success: function (response) {
+                if (response.status) {
+                    toast.success({message: response.message});
+                } else {
+                    toast.error({message: response.message ?? 'An error occurred'});
+                }
+            },
+            error: function (xhr, status, error) {
+                toast.error({message: 'An error occurred'});
+            }
+        });
+    });
+});
