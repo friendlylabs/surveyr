fa24e19f15fc7dda37ad86140b054a590b6ab064
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-24 02:34:35 +0300

    fix: clean up edit form view and optimize script loading

--- app/views/app/forms/edit.blade.php
+++ app/views/app/forms/edit.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.form.main')
-
 @style('/vendor/surveyjs/survey-creator-core.min.css','src')
 
 @section('content')
-
     <div id="surveyCreator" style="height: 100vh;"></div>
-    
 @endsection()
 
+@script('/vendor/surveyjs/surveyjs-widgets.min.js','src')
 @script('/vendor/surveyjs/survey-creator-core.min.js','src')
 @script('/vendor/surveyjs/dev/survey-creator-js.js','src')
 
