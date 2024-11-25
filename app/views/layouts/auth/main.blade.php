f8cffa147135aa4d9c0e589bafdb911c925834e7
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-25 19:17:49 +0300

    fix: add meta description for improved SEO and clarity

--- app/views/layouts/auth/main.blade.php
+++ app/views/layouts/auth/main.blade.php
@@ -7,6 +7,7 @@
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="msapplication-TileImage" content="/assets/img/favicons/mstile-150x150.png">
+		<meta name="description" content="An open-source and simple survey application for creating and managing surveys.">
 
         <link rel="shortcut icon" href="/favicon.ico">
 		<title>{{ _env('APP_NAME') }} {{ $title ?? 'App' }}</title>
@@ -22,7 +23,6 @@
 		<link href="/assets/vendors/simplebar/simplebar.min.css" rel="stylesheet">
 		<link href="/assets/css/theme.min.css" type="text/css" rel="stylesheet" id="style-default">
 		<link href="/assets/css/user.min.css" type="text/css" rel="stylesheet" id="user-style-default">
-		<link href="/assets/fonts/phosphor/duotone/style.css" rel="stylesheet"/>
         <link href="/assets/css/toast.min.css" rel="stylesheet">
 
         <link rel="manifest" href="/manifest.json">
