1c9e017a1017e3b04665f86e62608069114aabf3
Abdulbasit Rubeya <abdulbasitsultan4@gmail.com>
2024-11-25 20:54:54 +0300

    refactor: optimize recent submissions query and adjust limit to 5

--- app/models/Collection.php
+++ app/models/Collection.php
@@ -46,21 +46,34 @@ class Collection extends Model
     }
     
     # recent submissions
-    public static function recentSubmissions(int $userId, int $limit=10) : object
+    public static function recentSubmissions(int $userId, int $limit=5) : object
     {
-        $spaces = Space::where('user_id', $userId)
-            ->orWhereJsonContains('members', (string) $userId)->get()->pluck('id')->toArray();
-        if(!$spaces || !count($spaces)) $spaces = [0];
 
-        if(!$spaces) $spaces = [];
-        return static::where(function ($query) use ($userId, $spaces) {
+        # refactor: recent form submissions
+        # cols: formId, formName, submission:int, pending_review:int, last_submission
+        $spaces = Space::absoluteUserSpaces($userId);
+        return self::where(function ($query) use ($userId, $spaces) {
             $query->whereHas('form', function ($query) use ($userId, $spaces) {
                 $query->where('user_id', $userId)
-                    ->orWhereJsonContains('collaborators', (string) $userId)
-                    ->orWhereJsonContains('spaces', array_map('strval', $spaces));
+                      ->orWhereJsonContains('collaborators', (string) $userId)
+                      ->orWhereJsonContains('spaces', array_map('strval', $spaces));
             });
         })
-        ->orderBy('created_at', 'desc') // Order by created_at descending
+
+        # calculate submission count, pending review count, latest submission
+        ->select(
+            'form_id',
+            DB::$capsule::raw('COUNT(id) as submission'),
+            DB::$capsule::raw('SUM(review = "pending") as pending_review'),
+            DB::$capsule::raw('MAX(created_at) as latest_submission'))
+        
+        // form name, slug
+        ->with(['form' => function($query) {
+            $query->select('id', 'title', 'slug');
+        }])
+
+        ->orderBy('latest_submission', 'desc')
+        ->groupBy('form_id')
         ->limit($limit)
         ->get();
     }
