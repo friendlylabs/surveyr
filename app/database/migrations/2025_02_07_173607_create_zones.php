dfa820a2266f33793d9c1842c3021677bd98d2ba
ibnsultan <abdulbasitsultan4@gmail.com>
2025-02-13 19:55:56 +0300

    feat: create migration for zones table

---
+++ app/database/migrations/2025_02_07_173607_create_zones.php
@@ -0,0 +1,37 @@
+<?php
+
+use Leaf\Schema;
+use Leaf\Database;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateZones extends Database
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        if (!static::$capsule::schema()->hasTable('zones')) :
+            static::$capsule::schema()->create('zones', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name', 50);
+                $table->string('description', 50);
+                $table->json('content')->nullable();
+                $table->integer('user_id');
+                $table->json('editors')->nullable();
+                $table->timestamp('created_at')->useCurrent();
+                $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            });
+        endif;
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        static::$capsule::schema()->dropIfExists('zones');
+    }
+}
