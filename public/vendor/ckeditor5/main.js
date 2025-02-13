716ebe7a4a9a5d721d32e4a00c7fafb5ca54ef19
ibnsultan <abdulbasitsultan4@gmail.com>
2025-02-13 19:51:03 +0300

    feat: add CKEditor 5 integration

---
+++ public/vendor/ckeditor5/main.js
@@ -0,0 +1,62 @@
+import {
+	BalloonEditor,
+	AutoLink,
+	Autosave,
+	BlockToolbar,
+	Bold,
+	Essentials,
+	Italic,
+	Link,
+	List,
+	ListProperties,
+	PageBreak,
+	Paragraph,
+	Title,
+	Markdown
+} from 'ckeditor5';
+
+/**
+ * Create a free account with a trial: https://portal.ckeditor.com/checkout?plan=free
+ */
+const LICENSE_KEY = 'GPL'; // or <YOUR_LICENSE_KEY>.
+
+const editorConfig = {
+	toolbar: {
+		items: ['bold', 'italic', '|', 'pageBreak', 'link', '|', 'bulletedList', 'numberedList'],
+		shouldNotGroupWhenFull: false
+	},
+	plugins: [AutoLink, Autosave, BlockToolbar, Bold, Essentials, Italic, Link, List, ListProperties, PageBreak, Paragraph, Title, Markdown],
+	blockToolbar: ['bold', 'italic', '|', 'link', '|', 'bulletedList', 'numberedList'],
+	initialData:initialData,	
+	licenseKey: LICENSE_KEY,
+	link: {
+		addTargetToExternalLinks: true,
+		defaultProtocol: 'https://',
+		decorators: {
+			toggleDownloadable: {
+				mode: 'manual',
+				label: 'Downloadable',
+				attributes: {
+					download: 'file'
+				}
+			}
+		}
+	},
+	list: {
+		properties: {
+			styles: true,
+			startIndex: true,
+			reversed: true
+		}
+	},
+	placeholder: 'Type or paste your content here!'
+};
+
+// BalloonEditor.create(document.querySelector('#formDescription'), editorConfig);
+let editorInstance;
+BalloonEditor.create(document.querySelector('#editor'), editorConfig).then(editor => {
+	editorInstance = editor;
+	window.editor = editor;
+}) .catch(error => {
+	console.error(error);
+});
