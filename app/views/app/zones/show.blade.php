d96a1d80c7573717c2de6d8cc2ca42085bede336
ibnsultan <abdulbasitsultan4@gmail.com>
2025-03-03 18:26:44 +0300

    feat: implement x-spreadsheet integration for zone data editing and remove legacy editor

--- app/views/app/zones/show.blade.php
+++ app/views/app/zones/show.blade.php
@@ -1,91 +1,19 @@
 @extends('layouts.app.main')
 
-@push('styles')
-    <style>
-        .tabulatorEditToolbar {
-        }
-
-        .btn-tabulator {
-            padding-top: 2px;
-            background-color: var(--phoenix-btn-bg);
-            border: 1px solid #d1d1d1;
-            border-radius: 5px;
-        }
-
-        .tcm-menu {
-            padding: 5px 10px;
-            cursor: pointer;
-        }
-
-        .tcm-menu:hover {
-            background-color: #f1f1f1;
-            border-radius: 4px;
-        }
-
-        .CodeMirror {
-            height: calc(100vh - 200px) !important;
-        }
-    </style>
-@endpush
-
 @section('content')
-    <div class="content pb-5">
-        <div class="container-fluid mb-3">
-            <div class="tabulatorEditToolbar">
-                <button class="btn-tabulator" id="history-undo"
-                    data-bs-toggle="tooltip" data-bs-placement="bottom" title="Undo">
-                    <i class="fa-solid fa-undo"></i>
-                </button>
-                <button class="btn-tabulator" id="history-redo"
-                    data-bs-toggle="tooltip" data-bs-placement="bottom" title="Redo">
-                    <i class="fa-solid fa-redo"></i>
-                </button>
-                
-                <!-- switch between code editor and table editor, fa-grid and fa-code -->
-                <button class="btn-tabulator" id="switch-editor" onclick="switchEditor()"
-                    data-bs-toggle="tooltip" data-bs-placement="bottom" title="Switch Editor">
-                    <i class="fa-solid fa-code"></i>
-                </button>
-                <button class="btn-tabulator" id="file-save-trigger"
-                    data-bs-toggle="tooltip" data-bs-placement="bottom" title="Save Data">
-                    <i class="fa-solid fa-save"></i>
-                </button>
-
-                <!-- #add-column, #add-row -->
-                <button class="btn-tabulator" id="add-column"
-                    data-bs-toggle="tooltip" data-bs-placement="bottom" title="Add Column">
-                    <i class="fa-solid fa-columns"></i>
-                </button>
-                <button class="btn-tabulator" id="add-row"
-                    data-bs-toggle="tooltip" data-bs-placement="bottom" title="Add Row">
-                    <i class="fa-solid fa-table"></i>
-                </button>
-
-                <button class="btn-tabulator" id="file-load-trigger">Import Data</button>
-            </div>
+    <div class="content pb-5" style="padding-top: 4rem; padding-left: 0; margin-left: -1rem;">
+        <div class="ps-3" style="width: 100vw; height: calc(100vh - 75px); overflow: hidden;">
+            <div id="xspreadsheet" class="w-100"></div>
         </div>
-
-        <div class="container-fluid" id="codeEditorContainer">
-            <div id="zoneDataEditor" style="height: calc(100vh - 200px);"></div>
-
-        </div>
-
-        <div class="container-fluid d-none" id="tableEditorContainer">
-            <div id="zoneDataTable"></div>
-        </div>
-
     </div>
 
     <script>
-        const zoneDataContent = @json($zone->content);
+        const zoneDataContent = @json($zone->sheet);
     </script>
 @endsection
 
-@style('/vendor/tabulator/tabulator@6.min.css','src')
-@script('/vendor/tabulator/tabulator@6.min.js','src')
+@style('https://unpkg.com/x-data-spreadsheet@1.1.5/dist/xspreadsheet.css','src')
+@script('https://unpkg.com/x-data-spreadsheet@1.1.5/dist/xspreadsheet.js','src')
 
-@style('/vendor/monaco/editor.main.min.css','src')
-@script('/vendor/monaco/loader.min.js','src')
-
-@script('app.zones.scripts.show')
+@style('app.zones.styles.editor')
 @script('app.zones.scripts.editor')
