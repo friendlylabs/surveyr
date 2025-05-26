47e71b97c1f7386e72a7c947e7df9206134806c9
ibnsultan <abdulbasitsultan4@gmail.com>
2025-05-26 12:16:08 +0300

    feat: add loading spinner for sheet saving process in show view

--- app/views/app/zones/show.blade.php
+++ app/views/app/zones/show.blade.php
@@ -7,6 +7,18 @@
         </div>
     </div>
 
+    <div id="sheetSaveLoader" data-bs-toggle="tooltip" data-bs-placement="left"
+        title="{{ __('Saving...') }}"
+         style="
+            position: absolute;
+            bottom: 3px;
+            left: 20px;
+            z-index: 10;
+            display: none;
+        ">
+        <i class="fa fa-spinner fa-spin"></i>
+    </div>
+
     <script>
         const zoneDataContent = @json($zone->sheet);
     </script>
