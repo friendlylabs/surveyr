67a1babb336203e634d17c8f54f1ff0b094dc153
ibnsultan <abdulbasitsultan4@gmail.com>
2025-06-06 01:02:39 +0300

    fix: update surveyJson initialization for improved data handling

--- app/views/app/forms/scripts/show.blade.js
+++ app/views/app/forms/scripts/show.blade.js
@@ -5,7 +5,7 @@
  */
 
 const surveyMode = `{{ $surveyMode }}`;
-const surveyJson = (`{!! json_encode($formContent) !!}`).replace(/^\s+|\s+$/gm, '').split('\n').join('');
+const surveyJson = @json($form->content);
 
 var formTheme = "{{ $form->theme }}";
 const SurveyThemeLight = SurveyTheme[formTheme + 'LightPanelless'];
